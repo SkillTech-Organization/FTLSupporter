612229d1da4aa851a9e2b60532a3e7aab972737c
Györgyi Antal <antal_gyorgyi@freemail.hu>
2022-10-19 21:55:35 +0200

    mögy

--- FTLApi/DTO/Request/FTLSupportRequest.cs
+++ FTLApi/DTO/Request/FTLSupportRequest.cs
@@ -1,15 +1,15 @@
 ﻿using FTLSupporter;
-using System.Text.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace FTLApi.DTO.Request
 {
     // Root myDeserializedClass = JsonSerializer.Deserialize<Root>(myJsonResponse);
     public class FTLSupportRequest
     {
-        [JsonPropertyName("taskList")]
+        [JsonProperty("taskList")]
         public List<FTLTask> TaskList { get; set; }
 
-        [JsonPropertyName("truckList")]
+        [JsonProperty("truckList")]
         public List<FTLTruck> TruckList { get; set; }
     }
 }
