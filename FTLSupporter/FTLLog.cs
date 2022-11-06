47b64d44d0d408e80132b75d80da46c60f6d0e1d
Jeremi <kdrisaac42@gmail.com>
2022-11-06 23:11:58 +0100

    TelemetryLogger and FTLQueueResponse update, refactor

---
+++ FTLSupporter/FTLLog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTLSupporter
+{
+    public class FTLLog
+    {
+        public string Timestamp { get; set; }
+
+        public string Type { get; set; }
+
+        public string Message { get; set; }
+    }
+}
