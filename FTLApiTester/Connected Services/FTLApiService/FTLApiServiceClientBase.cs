4c34e63af78c9bd014951183d10b41cb6a861b54
Jeremi <kdrisaac42@gmail.com>
2022-11-16 15:45:11 +0100

    FTLApiTester added

---
+++ FTLApiTester/Connected Services/FTLApiService/FTLApiServiceClientBase.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTLApiService
+{
+    public class FTLApiServiceClientBase
+    {
+        private readonly IConfiguration _configuration;
+
+        protected string BaseUrl;
+
+        public FTLApiServiceClientBase(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+    }
+}
---
+++ FTLApiTester/Services/ApiTesterService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTLApiTester.Services
+{
+    internal class ApiTesterService : IApiTesterService
+    {
+        public async Task DoWork(CancellationToken cancellationToken = default)
+        {
+            var args = Environment.GetCommandLineArgs();
+            ;
+        }
+    }
+}
